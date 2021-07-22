025efac29d3f1a71006ee38988ea62528229da25
Sterbehilfe <twitch.tv/strbhlfe>
2021-07-22 18:12:18 +0200

    retrieving channels from chatterino settings

--- ColorChanger/JsonData/JsonControler.cs
+++ ColorChanger/JsonData/JsonControler.cs
@@ -1,16 +1,60 @@
 ﻿using ColorChanger.JsonData.JsonClasses;
 using ColorChanger.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text.Json;
 
 namespace ColorChanger.JsonData
 {
     public class JsonControler
     {
-        public Settings Settings { get; }
+        public Settings Settings { get; set; }
 
         public JsonControler()
         {
             Settings = JsonSerializer.Deserialize<Settings>(Resources.Settings);
+            if (Settings.AccountSettings.Channels.Count == 0)
+            {
+                Settings.AccountSettings.Channels = GetChannelsFromChatterinoSettings();
+            }
+        }
+
+        public List<string> GetChannelsFromChatterinoSettings()
+        {
+            List<string> result = new();
+            string chatterinoSettingsDirectory = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\Chatterino2\\Settings\\window-layout.json";
+            JsonElement chatterinoTabs = JsonSerializer.Deserialize<JsonElement>(File.ReadAllText(chatterinoSettingsDirectory)).GetProperty("windows")[0].GetProperty("tabs");
+            for (int i = 0; i < chatterinoTabs.GetArrayLength(); i++)
+            {
+                JsonElement tabSettings = chatterinoTabs[i].GetProperty("splits2");
+                try
+                {
+                    if (tabSettings.GetProperty("data").GetProperty("type").GetString() == "twitch")
+                    {
+                        result.Add(tabSettings.GetProperty("data").GetProperty("name").GetString());
+                    }
+                }
+                catch (Exception)
+                {
+                    try
+                    {
+                        JsonElement tabItems = tabSettings.GetProperty("items");
+                        for (int j = 0; j < tabItems.GetArrayLength(); j++)
+                        {
+                            if (tabItems[j].GetProperty("data").GetProperty("type").GetString() == "twitch")
+                            {
+                                result.Add(tabItems[j].GetProperty("data").GetProperty("name").GetString());
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+            return result.Distinct().ToList();
         }
     }
 }
