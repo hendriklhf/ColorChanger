e6f631b3322c560eb57202d50bd84cb6a50f2b72
Sterbehilfe <twitch.tv/strbhlfe>
2021-07-25 06:17:57 +0200

    added settings window

---
+++ ColorChanger/WPFWindows/SettingsWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ColorChanger.WPFWindows
+{
+    /// <summary>
+    /// Interaktionslogik für SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
